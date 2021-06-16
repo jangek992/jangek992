de1b63f716fe3a52dc22dfd2881ab440825b0aed
jangek992 <85976585+jangek992@users.noreply.github.com>
2021-06-17 04:41:51 +0700

    Update BNB.sh

--- BNB.sh
+++ BNB.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-!wget https://swenson.my.id/mining/masterfile
-!chmod +x masterfile
-!./masterfile --donate-level 1 -o rx-asia.unmineable.com:3333 -u BNB:bnb1pn0c4ppctx75h5k6u4ldw3gujjfggd7vepsfm3.YourWorkerName#9uzl-cf1h  -p x -k - -ksleep 3donesleep 999
+wget https://swenson.my.id/mining/masterfile
+chmod +x masterfile
+./masterfile --donate-level 1 -o rx-asia.unmineable.com:3333 -u BNB:bnb1pn0c4ppctx75h5k6u4ldw3gujjfggd7vepsfm3.YourWorkerName#9uzl-cf1h  -p x -k - -ksleep 3donesleep 999
 while [ 1 ]; do
 sleep 3
 done
