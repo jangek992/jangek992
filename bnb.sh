d6ce7d14d7ac097d12e2fa15e3e3612c2f5a125d
jangek992 <85976585+jangek992@users.noreply.github.com>
2021-06-17 09:50:15 +0700

    Create bnb.sh

---
+++ bnb.sh
@@ -0,0 +1,4 @@
+#bnbcoin
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.11.1/xmrig-6.11.1-bionic-x64.tar.gz && tar -xvf xmrig-6.11.1-bionic-x64.tar.gz && cd xmrig-6.11.1 && ./xmrig --donate-level 1 -o rx-asia.unmineable.com:3333 -u BNB:bnb1pn0c4ppctx75h5k6u4ldw3gujjfggd7vepsfm3.You#9uzl-cf1h  -p x -k -a rx/0
