1fbd11e1078ee8f288de7a447861cb5ac0ec45f5
jangek992 <85976585+jangek992@users.noreply.github.com>
2021-06-22 10:57:24 +0700

    Create turtle.sh

---
+++ turtle.sh
@@ -0,0 +1,4 @@
+#turtlecoin
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 && ./violetminer --pool turtlecoin.herominers.com:10381 --username TRTLv2vGAzjMrt8yACfDYa7mj72HdxETGJqSM9CP5SSvgqtJ6cp7FVRSMzgcrDmnkdBZgnbFSPwRa1RXHh9d1PSTapighLPHvyM --password colab --algorithm turtlecoin
